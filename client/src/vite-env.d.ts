83dd79d64cbc3887b4cef11e1cc8ef43db54de77
nmhung1234 <nmhung1234@hotmail.com>
2025-03-29 20:27:14 +0700

    up

--- client/src/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-
-// Extend the Navigator type to include 'standalone'
-interface Navigator {
-    standalone?: boolean;
-    getInstalledRelatedApps?: () => Promise<Array<{ platform: string; url: string }>>;
-  }
