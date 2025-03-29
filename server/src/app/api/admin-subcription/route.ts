d5fdc5114d3926d78bb7351442a271537948742a
nmhung1234 <nmhung1234@hotmail.com>
2025-03-29 21:18:26 +0700

    s

--- server/src/app/api/admin-subcription/route.ts
+++ server/src/app/api/admin-subcription/route.ts
@@ -3,18 +3,24 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
   // Handle GET request
-  const dbData: DataFileSync<{
-    subscriptions: { subscription: PushSubscription; deviceID: string }[];
-  }> = new DataFileSync('/public/db.json', {
-    parse: data => JSON.parse(data),
-    stringify: data => JSON.stringify(data),
-  });
-  const dataInDB = await dbData.read();
-  if (!dataInDB) {
-    return NextResponse.json({ subscriptions: [] }, { status: 200 });
-  } else
-    return NextResponse.json(
-      { subscriptions: dataInDB.subscriptions },
-      { status: 200 },
-    );
+  try {
+    const dbData: DataFileSync<{
+      subscriptions: { subscription: PushSubscription; deviceID: string }[];
+    }> = new DataFileSync('/public/db.json', {
+      parse: data => JSON.parse(data),
+      stringify: data => JSON.stringify(data),
+    });
+
+    const dataInDB = dbData.read();
+    console.log('🚀 ~ GET ~ dataInDB:', dataInDB);
+    if (!dataInDB) {
+      return NextResponse.json({ subscriptions: [] }, { status: 200 });
+    } else
+      return NextResponse.json(
+        { subscriptions: dataInDB.subscriptions },
+        { status: 200 },
+      );
+  } catch (error) {
+    console.log('🚀 ~ GET ~ error:', error);
+  }
 }
