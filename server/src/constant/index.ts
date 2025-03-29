3fc967ba291f600d84ed404948653d4b24a4876a
nmhung1234 <nmhung1234@hotmail.com>
2025-03-29 22:44:59 +0700

    te

--- server/src/constant/index.ts
+++ server/src/constant/index.ts
@@ -3,4 +3,4 @@ import { PushSubscription } from 'web-push';
 export const dbTemplate: {
   subscriptions: { subscription: PushSubscription; deviceID: string }[];
 } = { subscriptions: [] };
-export const dbFileLink = process.cwd() + '/public/db.json';
+export const dbFileLink = process.cwd() + '/public/static/db.json';
